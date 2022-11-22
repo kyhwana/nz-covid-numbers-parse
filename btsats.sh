46097b2e32605dd8361d176a9d7d55d9ac4a88ec
Kyhwana Pardus <kyhwana@gmail.com>
2022-11-23 11:09:20 +1300

    Update btsats.sh
    Add date field (UTC) and append to file

--- btsats.sh
+++ btsats.sh
@@ -1,3 +1,3 @@
 curl https://enf.tracing.covid19.govt.nz/api/pages/stats -o btstats.json
 echo "BT stats"
-jq ".dashboardItems[][].value" btstats.json | sed 's/\"//g' | sed 's/,//g' | paste -sd',' >btstatslatest.txt
+jq ".generated,.dashboardItems[][].value" btstats.json | sed 's/\"//g' | sed 's/,//g' | paste -sd',' >>btstatslatest.txt
